f18802c040cf0bff8ec59581c7b1e05b5d3481ad
dam1 <dam1@1DAM-05.IES.QUEVEDO-AD>
2016-02-15 12:32:40 +0100

    Hasta movimiento

---
+++ Ajedrez/src/ajedrez/Posicion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez;
+
+/**
+ *
+ * @author dam1
+ */
+public class Posicion {
+
+    private int fila;
+    private int columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+}
